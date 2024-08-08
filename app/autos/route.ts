abc526d494fe103f71a0ce63694da0ed4a528832
kovbasiukVitalina <vitalina.k@tvory.tech>
2024-08-08 18:40:30 +0300

    add

---
+++ app/autos/route.ts
@@ -0,0 +1,39 @@
+import { prisma } from "@/lib/prismadb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET() {
+  const invites = await prisma.auto.findMany({
+    include: {
+      Recipient: true,
+      Invite: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return NextResponse.json(invites, { status: 200 });
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+
+  const auto = await prisma.auto.create({
+    data: {
+      model: data.model,
+      brend: data.brend,
+      seats: data.seats,
+      class: data.class,
+      price: data.price,
+      inviteId: data.inviteId,
+      Recipient: {
+        create: data.Recipient.map((recipient: any) => ({
+          ...recipient,
+          inviteId: data.inviteId,
+        })),
+      },
+    },
+  });
+
+  return NextResponse.json(auto, { status: 201 });
+}
